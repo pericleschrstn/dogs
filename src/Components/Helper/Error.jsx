1a3aa5da7ffb8a4c0c8b68ed6e28aa6d8b144863
Péricles Christian <pericleschrstn@gmail.com>
2023-12-15 10:22:59 -0300

    useFetch login

--- src/Components/Helper/Error.jsx
+++ src/Components/Helper/Error.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Error = ({ error }) => {
   if (!error) return null;
+  console.log(error);
   return <p style={{ color: "#f31", margin: "1rem 0" }}>{error}</p>;
 };
 
