ab7f234d012794b890867a9467216dcbb59e2abb
Péricles Christian <pericleschrstn@gmail.com>
2023-12-19 10:39:21 -0300

    User Header

---
+++ src/Components/User/UserHeaderNav.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import MinhasFotos from "../../assets/feed.svg?react";
+import Estatisticas from "../../assets/estatisticas.svg?react";
+import AdicionarFoto from "../../assets/adicionar.svg?react";
+import Sair from "../../assets/sair.svg?react";
+import styles from "./UserHeaderNav.module.css";
+
+const UserHeaderNav = () => {
+  const [mobile, setMobile] = React.useState(null);
+  const { userLogout } = React.useContext(UserContext);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    userLogout();
+    navigate("/login");
+  }
+
+  return (
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end>
+        <MinhasFotos />
+        {mobile && "Minhas Fotos"}
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        {mobile && "Estatistícas"}
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <AdicionarFoto />
+        {mobile && "Adicionar Foto"}
+      </NavLink>
+      <button onClick={handleLogout}>
+        <Sair />
+        {mobile && "Sair"}
+      </button>
+    </nav>
+  );
+};
+
+export default UserHeaderNav;
